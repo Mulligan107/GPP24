773eccfa1f8562c53bdd15e4276355355f2530b0
Maxi <100299361+Mulligan107@users.noreply.github.com>
2024-06-16 18:14:55 +0200

    Player Movement Fixed

--- ShooterGame/src/handler/InputHandler.cs
+++ ShooterGame/src/handler/InputHandler.cs
@@ -12,7 +12,11 @@ namespace ShooterGame
     class InputHandler
     {
         //Event handler
-        static SDL.SDL_Event e;
+        private static SDL.SDL_Event _e;
+        
+        // Set of keys currently being pressed
+        static HashSet<SDL.SDL_Keycode> pressedKeys = new HashSet<SDL.SDL_Keycode>();
+
 
         public InputHandler() { 
             // Load the sound files
@@ -21,25 +25,38 @@ namespace ShooterGame
 
         public static (double, double, int ,string) handleUserInput()
         {
+            double s = Program.SCREEN_WIDTH / Program.SCREEN_HEIGHT;
             
-            while (SDL.SDL_PollEvent(out e) != 0)
+            while (SDL.SDL_PollEvent(out _e) != 0)
             {
+                // SDL_KEYDOWN ARE SDL_KEYUP ARE IMPORTANT FOR PLAYER MOVEMENT!!!
+                // Handle key press event
+                if (_e.type == SDL.SDL_EventType.SDL_KEYDOWN)
+                {
+                    pressedKeys.Add(_e.key.keysym.sym);
+                }
+
+                // Handle key release event
+                if (_e.type == SDL.SDL_EventType.SDL_KEYUP)
+                {
+                    pressedKeys.Remove(_e.key.keysym.sym);
+                }
 
                 //User requests quit via closing the window or pressing esc
-                if (e.type == SDL.SDL_EventType.SDL_QUIT || e.key.keysym.sym == SDL.SDL_Keycode.SDLK_ESCAPE)
+                if (_e.type == SDL.SDL_EventType.SDL_QUIT || _e.key.keysym.sym == SDL.SDL_Keycode.SDLK_ESCAPE)
                 {
                     Program.reset = true;
                     Program.quit = true;
                 }
 
                 //User requests quit via closing the window or pressing esc
-                if (e.type == SDL.SDL_EventType.SDL_QUIT || e.key.keysym.sym == SDL.SDL_Keycode.SDLK_r)
+                if (_e.type == SDL.SDL_EventType.SDL_QUIT || _e.key.keysym.sym == SDL.SDL_Keycode.SDLK_r)
                 {
                     Program.reset = true;
                 }
                 
                 //User requests pause via pressing 'p'
-                if (e.key.keysym.sym == SDL.SDL_Keycode.SDLK_p)
+                if (_e.key.keysym.sym == SDL.SDL_Keycode.SDLK_p)
                 {
                     if (Program.CurrentState == GameState.IN_GAME)
                     {
@@ -49,20 +66,20 @@ namespace ShooterGame
                 }
 
                 //Switch screen size mode if 'F' key was pressed
-                if (e.type == SDL.SDL_EventType.SDL_KEYDOWN) //ToDo könnte ein switch case sein
-                {
-                    // Change screen size
-                    if (e.key.keysym.sym == SDL.SDL_Keycode.SDLK_f)
-                    {
-                        Program.changeWindowSize();
-                    }
-                }
+                // if (e.type == SDL.SDL_EventType.SDL_KEYDOWN) //ToDo könnte ein switch case sein
+                // {
+                //     // Change screen size
+                //     if (e.key.keysym.sym == SDL.SDL_Keycode.SDLK_f)
+                //     {
+                //         Program.changeWindowSize();
+                //     }
+                // }
 
                 if (Program.CurrentState != GameState.IN_GAME)
                 {
-                    if (e.type == SDL.SDL_EventType.SDL_KEYDOWN)
+                    if (_e.type == SDL.SDL_EventType.SDL_KEYDOWN)
                     {
-                        switch (e.key.keysym.sym)
+                        switch (_e.key.keysym.sym)
                         {
                             case SDL.SDL_Keycode.SDLK_UP:
                                 Program.VisibleMenu.SelectPreviousItem();
@@ -81,26 +98,40 @@ namespace ShooterGame
                 }
                 
                 //If a key was pressed
-                if (e.type == SDL.SDL_EventType.SDL_KEYDOWN && e.key.repeat == 0 && Program.CurrentState == GameState.IN_GAME)
+                if (_e.type == SDL.SDL_EventType.SDL_KEYDOWN && _e.key.repeat == 0 && Program.CurrentState == GameState.IN_GAME)
                 {
-                    double s = Program.SCREEN_WIDTH / Program.SCREEN_HEIGHT;
                     double bulletspeed = 20;
-                    double movementspeed = 32;
+                    //double movementspeed = 32;
                     //Adjust the velocity
-                    switch (e.key.keysym.sym)
+                    switch (_e.key.keysym.sym)
                     {
                         case SDL.SDL_Keycode.SDLK_UP: return (0, (-bulletspeed * s),0, "shoot");
                         case SDL.SDL_Keycode.SDLK_DOWN: return (0, bulletspeed * s, 180, "shoot");
                         case SDL.SDL_Keycode.SDLK_LEFT: return (-bulletspeed * s, 0,-90, "shoot");
                         case SDL.SDL_Keycode.SDLK_RIGHT: return (bulletspeed * s, 0, 90, "shoot");
-                        case SDL.SDL_Keycode.SDLK_w: return (0, -movementspeed * s, 90, "move");
-                        case SDL.SDL_Keycode.SDLK_s: return (0, movementspeed * s, 90, "move");
-                        case SDL.SDL_Keycode.SDLK_a: return (-movementspeed * s, 0, 90, "move");
-                        case SDL.SDL_Keycode.SDLK_d: return (movementspeed * s, 0, 90, "move"); //Keine Rotation für das Schiff
-
+                        // case SDL.SDL_Keycode.SDLK_w: return (0, -movementspeed * s, 90, "move");
+                        // case SDL.SDL_Keycode.SDLK_s: return (0, movementspeed * s, 90, "move");
+                        // case SDL.SDL_Keycode.SDLK_a: return (-movementspeed * s, 0, 90, "move");
+                        // case SDL.SDL_Keycode.SDLK_d: return (movementspeed * s, 0, 90, "move"); //Keine Rotation für das Schiff
+                
                     }
                 }
             }
+            // Check for movement keys and return corresponding movement
+            double movementspeed = 16;
+            //double bulletspeed = 20;
+
+            if (pressedKeys.Contains(SDL.SDL_Keycode.SDLK_w)) return (0, -movementspeed * s, 90, "move");
+            if (pressedKeys.Contains(SDL.SDL_Keycode.SDLK_s)) return (0, movementspeed * s, 90, "move");
+            if (pressedKeys.Contains(SDL.SDL_Keycode.SDLK_a)) return (-movementspeed * s, 0, 90, "move");
+            if (pressedKeys.Contains(SDL.SDL_Keycode.SDLK_d)) return (movementspeed * s, 0, 90, "move");
+
+            // // Check for shooting keys and return corresponding command
+            // if (pressedKeys.Contains(SDL.SDL_Keycode.SDLK_UP)) return (0, -bulletspeed * s, 0, "shoot");
+            // if (pressedKeys.Contains(SDL.SDL_Keycode.SDLK_DOWN)) return (0, bulletspeed * s, 180, "shoot");
+            // if (pressedKeys.Contains(SDL.SDL_Keycode.SDLK_LEFT)) return (-bulletspeed * s, 0, -90, "shoot");
+            // if (pressedKeys.Contains(SDL.SDL_Keycode.SDLK_RIGHT)) return (bulletspeed * s, 0, 90, "shoot");
+
             return (0, 0, 0, "noAction");
         }
     }
