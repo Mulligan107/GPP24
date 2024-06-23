c617e56883888a0a7d65a88ad270b407ee47f1ea
Luggas <104451548+Bragandos@users.noreply.github.com>
2024-06-23 20:29:48 +0200

    merge with TileCS

---
+++ JumperGame/src/tilesets/Enviroment.tsx
@@ -0,0 +1,16 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Enviroment" tilewidth="16" tileheight="16" tilecount="256" columns="16">
+ <image source="sprites/world_tileset.png" trans="ff00ff" width="256" height="256"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="0" y="0" width="16" height="16"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="5">
+   <object id="9" x="0.0625" y="2.0625">
+    <polygon points="0,0 0,12.875 0.875,13.9375 13.875,13.9375 15.875,11.8125 15.9375,-1.25 14.8125,-2 2,-2.0625"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
