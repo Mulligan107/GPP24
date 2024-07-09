2fd28f81205e9b389d7d85d52d9bfe2c81bd24e9
Maxi <100299361+Mulligan107@users.noreply.github.com>
2024-07-09 09:43:22 +0200

    Create RenderComponent.cs

---
+++ JumperGame/src/components/RenderComponent.cs
@@ -0,0 +1,11 @@
+﻿namespace JumperGame.components;
+
+public class RenderComponent
+{
+    public LTexture Rendertexture { get; set; }
+    
+    public RenderComponent(LTexture lTexture)
+    {
+        Rendertexture = lTexture;
+    }
+}
