4cd33f76e3092e0a5014a4eb35ca61b82253e256
Maxi <100299361+Mulligan107@users.noreply.github.com>
2024-07-10 10:36:54 +0200

    Collision Component

---
+++ JumperGame/src/components/CollisionComponent.cs
@@ -0,0 +1,14 @@
+﻿using System.Numerics;
+
+namespace JumperGame.components
+{
+    public class CollisionComponent
+    {
+        public Vector2 Size { get; set; } // Width and Height of the entity for collision detection
+
+        public CollisionComponent(Vector2 size)
+        {
+            Size = size;
+        }
+    }
+}
